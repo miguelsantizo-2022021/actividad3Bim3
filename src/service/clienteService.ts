490601e437a3ea375a581373fff9b43d5049796d
MiguelSantizo <msantizo-2022021@kinal.edu.gt>
2026-07-07 21:51:51 -0600

    feat: se agrego el cliente service

--- src/service/clienteService.ts
+++ src/service/clienteService.ts
@@ -1,12 +1,13 @@
 import { Cliente } from '../models/cliente';
-import { PersistenciaService } from '../service/persistenciaService';
+import { PersistenciaService } from './persistenciaService';
 
 const ARCHIVO = 'clientes.json';
 
 export class ClienteService {
+
     private static validar(cliente: Cliente): void {
         if (!cliente.id || !cliente.nombreCompleto || !cliente.correo) {
-            throw new Error('ID, Nombre Completo y Correo son obligatorios.');
+            throw new Error('El ID, Nombre y Correo son obligatorios.');
         }
     }
 
@@ -17,14 +18,14 @@ export class ClienteService {
             const clientes = await PersistenciaService.leerDesdeArchivo<Cliente>(ARCHIVO);
             
             if (clientes.some(c => c.id === nuevoCliente.id)) {
-                throw new Error(`El cliente con ID ${nuevoCliente.id} ya existe.`);
+                throw new Error('Ese ID de cliente ya existe.');
             }
 
             clientes.push(nuevoCliente);
             await PersistenciaService.guardarEnArchivo(ARCHIVO, clientes);
-            console.log(`Cliente "${nuevoCliente.nombreCompleto}" guardado con éxito.`);
+            console.log('Cliente guardado con exito.');
         } catch (error: any) {
-            console.error(`Error al agregar cliente: ${error.message}`);
+            console.error('No se pudo guardar el cliente: ', error.message);
         }
     }
 
